d0801cdce6f305e8f86c337d1b555fd1fab4c81d
unknown <thusitha.4t@gmail.com>
2015-12-29 22:28:12 +0530

    pass data to view

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -7,4 +7,9 @@ class Controller
 		require_once '../app/models/' . $model . '.php';
 		return new $model();
 	}
+
+	public function view($view, $data = '')
+	{
+		require_once '../app/views/' . $view . '.php';
+	}
 }
